cd9cba894fd940cadf670d532c95a154ac50b435
Kal <Kal@DESKTOP-L318E7J>
2022-02-28 00:05:19 +0300

    changed constant url

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000/api/v1";
+export const API_URL = "https://temariyeback.herokuapp.com/api/v1";
 export const TOKEN_KEY = "token";
 export const USER_KEY = "USER";
 export const PUBLIC_URLS = {
